8200388ddffc090772e3c9b34e29a0b20e5153d3
Alexander Nesterovich <sasha.nest92@gmail.com>
2021-11-01 16:39:35 +0300

    Add InicializeHeader

--- ParserLib/WildBerriesParser.cs
+++ ParserLib/WildBerriesParser.cs
@@ -8,25 +8,36 @@ namespace Parser
     public class WildBerriesParser : IDisposable
     {
         private HttpClient _httpClient;
-        private HttpClientHandler _httpClientHandler;
-        private HttpRequestHeaders _httpRequestHeader;
         private string _product;
         private string SearchURL => "https://by.wildberries.ru/catalog/0/search.aspx?search=" + _product;
+        private HttpRequestHeaders Header => _httpClient.DefaultRequestHeaders;
 
         public WildBerriesParser(string product)
         {
             _httpClient = default;
-            _httpClientHandler = default;
-            _httpRequestHeader = default;
-            
             _product = product.Replace(" ", "+");
+
+            InicializeHeader();
+        }
+        private void InicializeHeader()
+        {
+            Header.Host = "by.wildberries.ru";
+            Header.Referrer = new Uri(SearchURL);
+            Header.UserAgent.Add(new ProductInfoHeaderValue("Mozilla", "5.0"));
+            Header.UserAgent.Add(new ProductInfoHeaderValue("Chrome", "93.0.4577.82"));
+            Header.AcceptLanguage.Add(new StringWithQualityHeaderValue("ru, en", 0.9));
+            Header.AcceptEncoding.Add(new StringWithQualityHeaderValue("gzip, deflate, br"));
         }
 
         private string GetXinfoFromWB()
         {
-            _httpRequestHeader
+            string pathQuery = "https://by.wildberries.ru/user/get-xinfo-v2";
+
+            var postResponce = 
+
             return "";
         }
+
 
         public void Dispose()
         {
