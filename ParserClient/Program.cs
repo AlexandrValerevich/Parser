7f2861861d6dfbeb27563bda93b9b3b004eae98b
Alexander Nesterovich <sasha.nest92@gmail.com>
2021-11-01 19:11:31 +0300

    complite the first step which linked with result

--- ParserClient/Program.cs
+++ ParserClient/Program.cs
@@ -2,14 +2,18 @@
 using System.Net;
 using System.IO;
 using System.Text;
+using System.Threading.Tasks;
 using static System.Console;
+using Parser;
 
 namespace Parser.Client
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            var wbParser = new WildBerriesParser("Angular 11");
+
             
         }
     }
