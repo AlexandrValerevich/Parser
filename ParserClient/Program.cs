9eb3de113782dd5530ef3af0f091652baea09232
Alexander Nesterovich <sasha.nest92@gmail.com>
2021-11-01 16:03:58 +0300

    Start

---
+++ ParserClient/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Text;
+using static System.Console;
+
+namespace Parser.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ ParserLib/WildBerriesParser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace Parser
+{
+    public class WildBerriesParser : IDisposable
+    {
+        private HttpClient _httpClient;
+        private HttpClientHandler _httpClientHandler;
+        private HttpRequestHeaders _httpRequestHeader;
+        private string _product;
+        private string SearchURL => "https://by.wildberries.ru/catalog/0/search.aspx?search=" + _product;
+
+        public WildBerriesParser(string product)
+        {
+            _httpClient = default;
+            _httpClientHandler = default;
+            _httpRequestHeader = default;
+            
+            _product = product.Replace(" ", "+");
+        }
+
+        private string GetXinfoFromWB()
+        {
+            _httpRequestHeader
+            return "";
+        }
+
+        public void Dispose()
+        {
+            _httpClient.Dispose();
+        }
+    }
+}
