5d6d6b70db7b987cd774cdb080e3cc7381aadacf
Alexander Nesterovich <sasha.nest92@gmail.com>
2021-11-02 21:00:53 +0300

    Add HttpClientBulder and remove unusfull file

---
+++ HttpFacade/HttpClientBulder.cs
@@ -0,0 +1,118 @@
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace HttpFacade
+{
+    class HttpClientBulder : IHttpClientBulder
+    {
+        private HttpClient _httpClient;
+        private HttpRequestHeaders _Header => _httpClient.DefaultRequestHeaders;
+        private Uri _Uri
+        {
+            get 
+            {
+                return  _httpClient.BaseAddress;
+            }
+            set
+            {
+                _httpClient.BaseAddress = value;
+            }
+        }
+
+        public IHttpClientBulder AddHeader(string key, string? value)
+        {
+            _Header.Add(key, value);
+            return this;
+        }
+
+        public IHttpClientBulder AddHeaderAcceptEncoding(string acceptEncoding)
+        {
+            _Header.AcceptEncoding.Add(new StringWithQualityHeaderValue(acceptEncoding));
+            return this;
+        }
+
+        public IHttpClientBulder AddHeaderAcceptEncoding(string acceptEncoding, double quality)
+        {
+            _Header.AcceptEncoding.Add(new StringWithQualityHeaderValue(acceptEncoding, quality));
+            return this;
+        }
+
+        public IHttpClientBulder AddHeaderAcceptLanguage(string language)
+        {
+            _Header.AcceptLanguage.Add(new StringWithQualityHeaderValue(language));
+            return this;
+        }
+
+        public IHttpClientBulder AddHeaderAcceptLanguage(string language, double quality)
+        {
+            _Header.AcceptLanguage.Add(new StringWithQualityHeaderValue(language,quality));
+            return this;
+        }
+
+        public IHttpClientBulder AddHeaderConnection(string connection)
+        {
+            _Header.Connection.Add(connection);
+            return this;
+        }
+
+        public IHttpClientBulder AddHeaderCookies(string cookies)
+        {
+            _Header.Add("Cookie", cookies);
+            return this;
+        }
+
+        public IHttpClientBulder AddHeaderHost(string host)
+        {
+            _Header.Host = host;
+            return this;
+        }
+
+        public IHttpClientBulder AddHeaderOrigin(string origin)
+        {
+            _Header.Add("Origin", origin);
+            return this;
+        }
+
+        public IHttpClientBulder AddHeaderReferer(string referer)
+        {
+            _Header.Referrer = new Uri(referer);
+            return this;
+        }
+
+        public IHttpClientBulder AddHeaderUserAgent(string userAgent)
+        {
+            _Header.UserAgent.Add(new ProductInfoHeaderValue(userAgent));
+            return this;
+        }
+
+        public IHttpClientBulder AddHeaderUserAgent(string userAgent, string version)
+        {
+             _Header.UserAgent.Add(new ProductInfoHeaderValue(userAgent, version));
+            return this;
+        }
+
+        public IHttpClientBulder AddUri(string uri)
+        {
+            _Uri = new Uri(uri);
+            return this;
+        }
+
+        public IHttpClientBulder AddUri(Uri uri)
+        {
+            _Uri = uri;
+            return this;
+        }
+
+        public HttpClient Build()
+        {
+            return _httpClient;
+        }
+
+        public void Reset()
+        {
+            _httpClient = new HttpClient();
+        }
+    }
+}
--- HttpFacade/IHttpClientBulder.cs
+++ HttpFacade/IHttpClientBulder.cs
@@ -10,13 +10,16 @@ namespace HttpFacade
         IHttpClientBulder AddUri(string uri);
         IHttpClientBulder AddUri(Uri uri);
         IHttpClientBulder AddHeader(string key, string value);
-        IHttpClientBulder AddHeaderHost(string URI);
-        IHttpClientBulder AddHeaderReferer(string URI);
+        IHttpClientBulder AddHeaderHost(string host);
+        IHttpClientBulder AddHeaderReferer(string referer);
         IHttpClientBulder AddHeaderConnection(string connection);
         IHttpClientBulder AddHeaderUserAgent(string userAgent);
-        IHttpClientBulder AddHeaderOrigin(string URI);
+        IHttpClientBulder AddHeaderUserAgent(string userAgent, string version);
+        IHttpClientBulder AddHeaderOrigin(string origin);
         IHttpClientBulder AddHeaderAcceptEncoding(string acceptEncoding);
+        IHttpClientBulder AddHeaderAcceptEncoding(string acceptEncoding, double quality);
         IHttpClientBulder AddHeaderAcceptLanguage(string language);
+        IHttpClientBulder AddHeaderAcceptLanguage(string language, double quality);
         IHttpClientBulder AddHeaderCookies(string cookies);
     }
 }
